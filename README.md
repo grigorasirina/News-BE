4977eb537856ae10afeb2fa71bbc63b4b25a1b30
irina <grigorasirina17@gmail.com>
2025-04-28 10:59:18 +0000

    article and comments tables added

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # NC News Seeding
 
-- Instructions for this sprint can be found at https://l2c.northcoders.com/courses/be/seeding-nc-news
+Step 1: Run the setup script
+To create both databases, run:
+npm run setup-dbs
+This will run the setup-dbs.sql file to create both a test and development database.
+Step 2: Set up environment variables
+.env.test (for the test database).
+.env.development (for the development database).
+Double-check that your .gitignore file includes .env.* so these files aren't pushed to GitHub.
+Step 3: Verify your setup
+Run the following scripts and check the console logs (you may need to scroll up a bit in the terminal!):
+npm run test-seed
+This runs tests for your seed function.
+No tests will pass yet (which is expected), but you should see logs confirming you are connected to your test database.
+npm run seed-dev
+This runs the run-seed script, which calls your seed function with development data.
+You will see errors at this stage, but the logs should confirm that you are connected to the development database.
+A seeds folder
+This contains the scripts for seeding your databases.
+Any utility functions we need for data-manipulation are also found here.
+connection.js file
+The connection has already been written for you and is exported from here. Both the seed.js file and its corresponding test suite are requiring in the connection already.
--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,6 +1,130 @@
-const db = require("../connection")
+const db = require("../connection");
+const format = require("pg-format")
+const { values } = require("../data/test-data/articles");
 
 const seed = ({ topicData, userData, articleData, commentData }) => {
-  return db.query(); //<< write your first query in here.
-};
+    //drop databases for topics, users, articles, comments
+    return db.query('DROP TABLE IF EXISTS comments')
+    .then(() => {
+    return db.query('DROP TABLE IF EXISTS articles')
+    .then(()=> {
+    return db.query('DROP TABLE IF EXISTS users')
+      })
+    .then(()=> {
+    return db.query('DROP TABLE IF EXISTS topics')
+    })
+
+
+    //create tables for topics, users, articles,comments
+    .then(()=> {
+    return db.query(`CREATE TABLE topics (
+      slug VARCHAR(40) PRIMARY KEY,
+      description VARCHAR(100),
+      img_url VARCHAR(1000));`)
+  })
+
+
+  .then(()=> {
+    return db.query(`CREATE TABLE users (
+      username VARCHAR(30) PRIMARY KEY ,
+      name VARCHAR (80),
+      avatar_url VARCHAR(1000))`)
+  })
+
+
+  .then(()=> {
+    return db.query(`CREATE TABLE articles (
+      article_ID SERIAL PRIMARY KEY,
+      title VARCHAR(100),
+      topic VARCHAR(40) REFERENCES topics(slug),
+      author VARCHAR(30) REFERENCES users(username) ,
+      body TEXT,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      votes INT DEFAULT 0,
+      article_img_url VARCHAR(1000)
+      )`)
+  })
+
+
+  .then(()=> {
+    return db.query(`CREATE TABLE comments (
+      comment_id SERIAL PRIMARY KEY,
+      article_id INT REFERENCES articles(article_ID),
+      body TEXT,
+      votes INT DEFAULT 0,
+      author VARCHAR(30) REFERENCES users(username),
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      )`)
+  })
+
+
+  .then(() => {
+    const formattedTopic = topicData.map((topic) => {
+      return [topic.description, topic.slug, topic.img_url]
+    })
+    const insertTopicsString = format(
+      `INSERT INTO topics
+      (description, slug, img_url)
+      VALUES
+      %L 
+      RETURNING *;`,
+      formattedTopic
+    )
+    return db.query(insertTopicsString)
+  })
+
+
+  .then(() =>{
+    const formattedUser = userData.map((user) => {
+      return [user.username, user.name, user.avatar_url]
+    })
+    const insertUserString = format(
+      `INSERT INTO users
+      (username, name, avatar_url)
+      VALUES
+      %L
+      RETURNING *;`,
+      formattedUser
+    )
+    return db.query(insertUserString)
+  })
+
+
+  .then(() => {
+    const formattedArticles = articleData.map((article) => {
+      const createdAtDate = new Date(article.created_at)
+      return [article.title, article.topic, article.author, article.body, createdAtDate, article.votes, article.article_img_url,]
+    })
+    const insertArticleString = format(
+      `INSERT INTO articles
+      (title, topic, author, body, created_at, votes, article_img_url)
+      VALUES
+      %L
+      RETURNING *;`,
+      formattedArticles
+    )
+    return db.query(insertArticleString)
+  })
+.then(() => {
+  
+})
+
+
+.then(() => {
+    const formattedComment = commentData.map((comment) => {
+       const createdAtDate = new Date(comment.created_at)
+      return [comment.article_id, comment.body, comment.votes, comment.author, createdAtDate]
+    })
+    const insertCommentString = format(
+      `INSERT INTO comments
+      (article_id, body, votes, author, created_at)
+      VALUES
+      %L 
+      RETURNING *;`,
+      formattedComment
+    )
+    return db.query(insertCommentString)
+  })
+})
+}
 module.exports = seed;
