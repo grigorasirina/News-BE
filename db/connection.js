32ed0b25bfe575d9a93bd0f0239743906a2b7d67
irina <grigorasirina17@gmail.com>
2025-05-16 09:30:29 +0000

    connection changed

--- db/connection.js
+++ db/connection.js
@@ -4,12 +4,11 @@ const ENV = process.env.NODE_ENV || 'development'
 
 require('dotenv').config({path: `${__dirname}/../.env.${ENV}`})
 
-const db = new Pool();
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
     throw new Error("No PGDATABASE configured")
 } else { 
-    console.log(`Connected to ${process.env.PGDATABASE}`)
+    console.log(`Connected to ${process.env.DATABASE_URL}`)
 }
 
 const config = {};
@@ -18,6 +17,5 @@ if (ENV === "production") {
   config.max = 2;
 }
 
-module.exports = db;
 module.exports = new Pool(config)
 
